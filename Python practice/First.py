51137d9f178af97ec1d04d1b4e0cf6279592a4d0
Arindam Das <dasarindam342@gmail.com>
2026-07-13 22:55:31 +0530

    Add files via upload

---
+++ Python practice/First.py
@@ -0,0 +1,5 @@
+# 1. Write a program to take a user’s name and age as input and print a greeting message.
+name = input("Enter your name : ") # arindam
+age = int(input("Enter your age: ")) #100
+
+print("Hello Sir/Ma'am",name,"Welcome to our restaurent we predict your age is : ", age) 
---
+++ Python practice/Second.py
@@ -0,0 +1,17 @@
+#2. Take two numbers as input and print their sum(+), difference(-), multiplication(*), and division(/).
+number1=int(input("Enter the first number:-- > ")) # ex 10
+number2=int(input("Enter the second number:--> "))
+
+sum = number1 + number2
+difference = number1 - number2
+multiplication = number1 * number2
+division = number1 / number2
+
+print("Sum is:",sum) # 100
+print("Difference is:",difference)#20
+print("Multiplecation is",multiplication)#2400
+print("Division",division)#1.5
+
+
+
+
